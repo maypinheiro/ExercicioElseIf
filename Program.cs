90c04e9bda6995bf8414c6d331d366fc9063c56a
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-09 14:18:13 -0300

    Envio do exercicio sobre if else

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ddd= int.Parse(Console.ReadLine());
+
+                 if (ddd == 61) Console.WriteLine("Brasilia");
+            else if (ddd == 71) Console.WriteLine("Salvador");
+            else if (ddd == 11) Console.WriteLine("São Paulo");
+            else if (ddd == 21) Console.WriteLine("Rio de Janeiro");
+            else if (ddd == 32) Console.WriteLine("Juíz de Fora");
+            else if (ddd == 19) Console.WriteLine("Campinas");
+            else if (ddd == 31) Console.WriteLine("Belo Horizonte");
+            else Console.WriteLine("DDD não cadastrado");
+
+            Console.ReadKey();
+        }
+    }
+}
