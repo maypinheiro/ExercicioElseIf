116992e65048b69960158890f1051152e1030b4c
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2022-08-09 14:49:34 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,24 @@ namespace Exercicio
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int ddd= int.Parse(Console.ReadLine());
-
-                 if (ddd == 61) Console.WriteLine("Brasilia");
+          static void  VerificaDDD(int ddd) {
+              
+            if (ddd == 61) Console.WriteLine("Brasilia");
             else if (ddd == 71) Console.WriteLine("Salvador");
             else if (ddd == 11) Console.WriteLine("São Paulo");
             else if (ddd == 21) Console.WriteLine("Rio de Janeiro");
             else if (ddd == 32) Console.WriteLine("Juíz de Fora");
             else if (ddd == 19) Console.WriteLine("Campinas");
             else if (ddd == 31) Console.WriteLine("Belo Horizonte");
+              
             else Console.WriteLine("DDD não cadastrado");
+        }
 
+        
+        static void Main(string[] args)
+        {
+            int ddd= int.Parse(Console.ReadLine());
+            VerificaDDD(ddd);
             Console.ReadKey();
         }
     }
